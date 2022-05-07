f2c59e69394c33218fe979398018c8df31aeccef
michaelsatumba <mjsatumba@gmail.com>
2022-05-07 11:52:17 -0700

    sign in auth

--- pages/Home.js
+++ pages/Home.js
@@ -5,28 +5,25 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 
 function Home() {
-	// const [user, setUser] = useState(null);
+
 	const router = useRouter();
 
-	const user = authentication.currentUser;
+	const [user, setUser] = useState();
 
-	if (user) {
-		console.log('signed in');
-		// console.log(user);
-		// router.push('/');
-	} else {
-		console.log('not signed in');
-		// router.push('/');
-	}
+	useEffect(() => {
+		onAuthStateChanged(authentication, (user) => {
+			if (user) {
+			  console.log('signed in');
+			  setUser(user)
+			} else {
+			  // User is signed out
+			  // ...
+			  console.log('not signed in');
+			  router.push('/');
+			}
+		  });
+	  }, []);
 
-	// const {
-	// 	query: { name, photo },
-	// } = router;
-
-	// const props = {
-	// 	name,
-	// 	photo,
-	// };
 
 	const logout = () => {
 		signOut(authentication)
@@ -41,18 +38,18 @@ function Home() {
 	return (
 		<div>
 			<div className="flex justify-evenly">
-				{/* <p>Welcome {props.name} </p>
+				<p>Welcome {user?.displayName} </p>
 
 				<button onClick={logout}>
 					<div className="h-14 w-14 relative rounded-lg">
 						<Image
-							src={props.photo}
+							src={user?.photoURL}
 							alt="userPhoto"
 							layout="fill"
 							className="rounded-full"
 						/>
 					</div>
-				</button> */}
+				</button>
 			</div>
 		</div>
 	);
