8dba97a07145525ac4a5db30513e492555f2b83a
michaelsatumba <mjsatumba@gmail.com>
2022-05-20 11:24:55 -0700

    className

--- pages/Home.js
+++ pages/Home.js
@@ -163,16 +163,16 @@ function Home() {
 					onSubmit={handleSubmit}
 					className="form p-2 flex flex-col items-center border-black"
 				>
-					<label class="flex flex-col items-center rounded-lg tracking-wide uppercase cursor-pointer bg-white hover:bg-blue-500 hover:text-white text-blue-500 px-16">
+					<label className="flex flex-col items-center rounded-lg tracking-wide uppercase cursor-pointer bg-white hover:bg-blue-500 hover:text-white text-blue-500 px-16">
 						<svg
-							class="w-8 h-8"
+							className="w-8 h-8"
 							fill="currentColor"
 							xmlns="http://www.w3.org/2000/svg"
 							viewBox="0 0 20 20"
 						>
 							<path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
 						</svg>
-						<span class="mt-2 text-base leading-normal">Select a file</span>
+						<span className="mt-2 text-base leading-normal">Select a file</span>
 						<input type="file" class="hidden" />
 					</label>
 
